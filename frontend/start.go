b251c0c4427638cf5c0de95fe9b9963ee2ad85cd
ui3u <119984735+mtmox@users.noreply.github.com>
2024-11-23 15:17:26 -0600

    ---

--- frontend/start.go
+++ frontend/start.go
@@ -3,8 +3,6 @@ package frontend
 
 import (
 	"log"
-	"fmt"
-	"encoding/json"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -12,9 +10,6 @@ import (
 	"fyne.io/fyne/v2/widget"
 
 	"github.com/nats-io/nats.go"
-
-	"github.com/mtmox/AI-cluster/streams"
-	"github.com/mtmox/AI-cluster/constants"
 )
 
 // Add this global variable
@@ -41,31 +36,3 @@ func StartFrontend(js nats.JetStreamContext, logger *log.Logger) {
 	w.ShowAndRun()
 }
 
-func configSyncModels(js nats.JetStreamContext, logger *log.Logger) error {
-	subject := "config.sync.models"
-	durable := "config_sync_models"
-	
-	_, err := streams.DurablePull(js, "nodes", subject, durable, func(msg *nats.Msg) {
-		populateModels(msg, logger)
-	})
-	if err != nil {
-		return fmt.Errorf("Failed to set up consumer to populate models: %s: %v", subject, err)
-	}
-	logger.Printf("Consumer set up for subject: %s", subject)
-	return nil
-}
-
-func populateModels(msg *nats.Msg, logger *log.Logger) {
-    var model constants.ConfigSyncModels
-	err := json.Unmarshal(msg.Data, &model)
-	if err != nil {
-		logger.Printf("Error unmarshaling model data: %v", err)
-		return
-	}
-	
-	if !modelNames[model.Name] {
-		modelNames[model.Name] = true
-		logger.Printf("Added new model: %s", model.Name)
-		updateModelSelector()
-	}
-}
