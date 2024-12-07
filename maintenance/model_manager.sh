f7ee447b89adb80c4b5f799a430d1d012538dc9b
ui3u <119984735+mtmox@users.noreply.github.com>
2024-12-07 00:44:39 -0600

    ---

---
+++ maintenance/model_manager.sh
@@ -0,0 +1,4 @@
+// This file should be use to push commands to update ollama on all computers or individual,
+// or tell all computers or individual to download and or delete models. Use IP in the header
+// and if a computer checks its IP against the header and matches then the computer should 
+// process the message. If there is no header then it applies to all computers.
